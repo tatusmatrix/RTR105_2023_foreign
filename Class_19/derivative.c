4e2bdd6b51c7cd6427c536d79c943f1a60d84d15
tatusmatrix <Tatjana.Solovjova@rtu.lv>
2023-11-28 14:07:29 +0200

    Class_19

---
+++ Class_19/derivative.c
@@ -0,0 +1,24 @@
+#undef __STRICT_ANSI__
+
+#include <math.h>
+#include <stdio.h>
+
+int main() {
+  FILE *pFile;
+  float a = 0., b = 2 * M_PI, x, delta_x = 1.e-2;
+  // float y;
+  pFile = fopen("derivative.dat", "w");
+  // printf("\tx\t\tsin(x)\t\tsin\'(x)\n");
+  fprintf(pFile, "\tx\t\tsin(x)\t\tsin\'(x)\n");
+  x = a;
+  while (x < b) {
+    // y=sin(x);
+    // printf("%10.2f\t%10.2f\t%10.2f\n",x,sin(x),(sin(x+delta_x)-sin(x))/delta_x);
+    fprintf(pFile, "%10.2f\t%10.2f\t%10.2f\n", x, sin(x),
+            (sin(x + delta_x) - sin(x)) / delta_x);
+    // printf(”%10.2f\t%10.2f\n”,x,y);
+    x += delta_x; // x=x+delta_x;
+  }
+  fclose(pFile);
+  return 0;
+}
